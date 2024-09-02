3f9a8fe0db45edafbee510f71b73e45d3c6156a2
NUNIYA <nuniyat.g@gmail.com>
2024-09-02 14:39:44 +0300

    corrected the error on contoller

--- server/controllers/dashboardcontroller.js
+++ server/controllers/dashboardcontroller.js
@@ -56,11 +56,12 @@ exports.dashboard = async (req, res) => {
     .where({user:req.user.id}).lean();
 
     if(note){
-      res.render('dashboard/view-note',{
-     noteID:req.params.id,
-     note,
-     layout:'../views/layouts/dashboard'
-      });
+      res.render('dashboard/view-notes', {
+        noteID: req.params.id,
+        note,
+        layout: '../views/layouts/dashboard'
+    });
+    
     }else{
       res.send("something went wrong.")
     }
