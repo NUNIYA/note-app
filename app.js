678460bf7118a784be6461f1fe5387f5eee6b64f
NUNIYA <nuniyat.g@gmail.com>
2024-09-02 21:10:31 +0300

    added cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 
+const cors = require('cors');
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const methodOverride = require("method-override");
@@ -11,7 +12,10 @@ const MongoStore = require('connect-mongo');
 const app = express();
 const port = process.env.PORT || 10000;
 
+// Apply CORS middleware
+app.use(cors());
 
+// Set up session middleware
 app.use(session({
   secret: 'keyboard cat',
   resave: false,
@@ -21,9 +25,11 @@ app.use(session({
   })
 }));
 
+// Initialize Passport
 app.use(passport.initialize());
 app.use(passport.session());
 
+// Parse incoming requests
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(methodOverride("_method"));
@@ -31,26 +37,25 @@ app.use(methodOverride("_method"));
 // Connect to Database
 connectDB();
 
-// Static Files
+// Serve static files
 app.use(express.static('public'));
 
-
-// Templating Engine
+// Set up templating engine
 app.use(expressLayouts);
 app.set('layout', './layouts/main');
 app.set('view engine', 'ejs');
 
-// Routes
+// Define routes
 app.use('/', require('./server/routes/auth'));
 app.use('/', require('./server/routes/index'));
 app.use('/', require('./server/routes/dashboard'));
 
-// Handle 404
+// Handle 404 errors
 app.use((req, res) => {
   res.status(404).render('404');
 });
 
+// Start the server
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
-}); 
-
+});
