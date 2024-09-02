e9974374b8e7f279a8fde5062d0e3975ba0282b2
NUNIYA <nuniyat.g@gmail.com>
2024-09-02 20:48:26 +0300

    chnage port

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ const passport = require('passport');
 const MongoStore = require('connect-mongo');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
+
 
 app.use(session({
   secret: 'keyboard cat',
